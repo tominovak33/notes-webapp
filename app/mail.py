313e9345d1f7b7e1fcf117228f95fe4b5ca2ed16
Tomi <dev.tomi33@gmail.com>
2016-08-07 23:54:04 +0100

    email templates names

--- app/mail.py
+++ app/mail.py
@@ -4,22 +4,27 @@ from sendgrid.helpers.mail import *
 
 import config
 
+# template_name: template_id
+# template_name is for python code only, template_id is set in sendgrid
+email_templates = {
+    'test_template': '06ac801d-a03f-4c1a-bec3-0a4d4ac6b867',
+}
+
 sg = sendgrid.SendGridAPIClient(apikey=config.SENDGRID_API_KEY)
 
 
-def send_mail(to, subject, content, template_id=False):
+def send_mail(to, subject, content, template_name=False):
     from_email = Email(config.SENDGRID_FROM_EMAIL_ADDRESS)
 
     to_email = Email(to)
     email_content = Content("text/html", content)
     mail = Mail(from_email, subject, to_email, email_content)
-    if template_id:
-        mail.set_template_id('06ac801d-a03f-4c1a-bec3-0a4d4ac6b867')
+    if template_name:
+        mail.set_template_id(email_templates.get(template_name))
     response = sg.client.mail.send.post(request_body=mail.get())
-    print response
     return response
 
 # Example email sending
 # Import mail
-# mail.send_mail("destination@example.com", "My subject", "The test content", "06ac801d-a03f-4c1a-bec3-0a4d4ac6b867")
-
+# mail.send_mail("destination@example.com", "My subject", "The test content", "test_template")
+# ^ where 'test-template' is in the email_templates above
