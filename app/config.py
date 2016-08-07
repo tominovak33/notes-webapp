b823c7879be0407b59f387fba4e890e433cd034c
Tomi <dev.tomi33@gmail.com>
2016-08-08 00:07:08 +0100

    sendgrid tweak

--- app/config.py
+++
@@ -1,6 +0,0 @@
-JWT_SECRET_KEY = 'my-secret-jwt-key-goes-here'
-
-SENDGRID_API_KEY = "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"
-
-SENDGRID_FROM_EMAIL_ADDRESS = "noreply@tomi33.co.uk"
-
